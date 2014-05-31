3a60d51a002ede951fa00e4dd78a3982132c0ff7
Boyoung Kwon <boyoung.kwon@gmail.com>
2014-05-31 14:31:22 -0400

    add amazon s3 login information

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module AutoMusicVideo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    AWS::S3::Base.establish_connection!(
+        :access_key_id => 'AKIAICRCZ64QDGNZ4B7A'
+        :secret_access_key => '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'
+    )
+
+    BUCKET = 'automusicvideosongs'
   end
 end
